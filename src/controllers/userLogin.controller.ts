195a8ca9acffb87a5ad815e74530f0820bdcc70d
Jhon Muller <jmuller.jhon80@gmail.com>
2022-07-26 10:32:14 -0400

    fix: id no controller login

--- src/controllers/userLogin.controller.ts
+++ src/controllers/userLogin.controller.ts
@@ -4,9 +4,9 @@ import userLoginService from "../services/userLogin.service";
 const userLoginController = async (req: Request, res: Response) => {
     try {
         const {email, password} = req.body;
-        const token = await userLoginService({email, password});
+        const id = await userLoginService({email, password});
 
-        return res.status(201).json({token});
+        return res.status(201).json({id});
     } catch (error) {
         if(error instanceof Error){
             return res.status(401).send({
