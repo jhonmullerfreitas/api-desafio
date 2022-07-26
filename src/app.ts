9dfb0b3ddab10220d6999770769b2268b5ed66de
Jhon Muller <jmuller.jhon80@gmail.com>
2022-07-26 08:54:50 -0400

    feat: deploy

--- src/app.ts
+++ src/app.ts
@@ -7,4 +7,4 @@ app.use(express.json())
 
 app.use(routes)
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
--- src/data-source.ts
+++ src/data-source.ts
@@ -1,20 +1,21 @@
 import { DataSource, Migration } from "typeorm";
-import {User} from "./entities/user.entity"
 require("dotenv").config();
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PWD,
-    database: process.env.POSTGRES_DB,
+    url: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === "production" ?
+        { rejectUnauthorized: false}
+        : false,
 
     synchronize: false,
     logging: true,
-    entities: [User],
-    migrations: ["src/migrations/*.ts"]
+    entities: process.env.NODE_ENV === "production"
+        ? ["dist/entities/*.js"]
+        : ["src/entities/*.ts"],
+    migrations: process.env.NODE_ENV === "production"
+        ? ["dist/migrations/*.js"]
+        : ["src/migrations/*.ts"],
 })
 
 AppDataSource.initialize().then(()=> {
